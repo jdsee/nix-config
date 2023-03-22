7774d3b7828f1768ede615e4d186cfb9a33d0d7c
jdsee <joscha-seelig@outlook.de>
2023-03-22 22:50:30 +0100

    Add some swaylock options

--- home/jdsee/features/desktop/common/wayland-wm/swaylock.nix
+++ home/jdsee/features/desktop/common/wayland-wm/swaylock.nix
@@ -4,14 +4,16 @@
   home.packages = [ pkgs.swaylock-effects ];
   programs.swaylock = {
     settings = {
-      # effect-blur = "20x3";
+      effect-blur = "20x3";
+      effect-scale = 0.3;
       fade-in = 0.1;
-
+      screenshots = true;
+      clock = true;
       font = config.fontProfiles.regular.family;
       font-size = 15;
-
       line-uses-inside = true;
       disable-caps-lock-text = true;
+      indicator = true;
       indicator-caps-lock = true;
       indicator-radius = 40;
       indicator-idle-visible = true;
