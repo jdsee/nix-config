7d0f6acf9180c3d4651d9194c3e8c6c2e4dba310
jdsee <joscha-seelig@outlook.de>
2023-03-22 22:50:50 +0100

    Add some make options

--- home/jdsee/features/desktop/common/wayland-wm/mako.nix
+++ home/jdsee/features/desktop/common/wayland-wm/mako.nix
@@ -2,12 +2,12 @@
 {
   programs.mako = {
     enable = true;
-    iconPath = "${config.gtk.iconTheme.package}/share/icons/Papirus-Dark";
+    # iconPath = "${config.gtk.iconTheme.package}/share/icons/Papirus-Dark";
     font = "${config.fontProfiles.regular.family} 12";
     padding = "10,20";
     anchor = "top-right";
-    width = 400;
-    height = 150;
+    # width = 400;
+    # height = 150;
     borderSize = 2;
     defaultTimeout = 12000;
     # backgroundColor = "#${colors.base00}dd";
