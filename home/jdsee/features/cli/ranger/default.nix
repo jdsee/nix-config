287d88bc72f5b07bac5a089e808e1b3ea2f0f63e
jdsee <joscha-seelig@outlook.de>
2023-03-31 01:06:58 +0200

    Add zoxide plugin to ranger

--- home/jdsee/features/cli/ranger/default.nix
+++ home/jdsee/features/cli/ranger/default.nix
@@ -9,5 +9,13 @@
       source = ./config;
       recursive = true;
     };
+    "ranger/plugins/zoxide" = {
+      source = pkgs.fetchFromGitHub {
+        owner = "jchook";
+        repo = "ranger-zoxide";
+        rev = "363df97af34c96ea873c5b13b035413f56b12ead";
+        sha256 = "sha256-HFCGuAektrSHF7GJsoKv1TJ/T/dwkYK1eUcw8nEjJII=";
+      };
+    };
   };
 }
