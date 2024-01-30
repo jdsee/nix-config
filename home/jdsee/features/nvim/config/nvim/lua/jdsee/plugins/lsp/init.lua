ec861ad17570d8d294bc126d7589a43d143e8099
jdsee <jdsee@protonmail.com>
2024-01-31 00:25:21 +0100

    Replace Rustaceanvim with rust-tools

--- home/jdsee/features/nvim/config/nvim/lua/jdsee/plugins/lsp/init.lua
+++ home/jdsee/features/nvim/config/nvim/lua/jdsee/plugins/lsp/init.lua
@@ -1,7 +1,7 @@
 return {
   require 'jdsee.plugins.lsp.zero',
   require 'jdsee.plugins.lsp.cmp',
-  require 'jdsee.plugins.lsp.rust-tools',
+  require 'jdsee.plugins.lsp.rustaceanvim',
   require 'jdsee.plugins.lsp.trouble',
   require 'jdsee.plugins.lsp.typst',
 }
