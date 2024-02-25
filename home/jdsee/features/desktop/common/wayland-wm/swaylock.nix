a2d4da3069a71f9c5678166614b5b768941611de
jdsee <jdsee@protonmail.com>
2024-02-25 02:25:09 +0100

    Update swaylock styles

--- home/jdsee/features/desktop/common/wayland-wm/swaylock.nix
+++ home/jdsee/features/desktop/common/wayland-wm/swaylock.nix
@@ -1,22 +1,52 @@
 { config, pkgs, ... }:
 
 {
-  # home.packages = [ pkgs.swaylock-effects ];
   programs.swaylock = {
     package = pkgs.swaylock-effects;
     settings = {
       image = "$HOME/.config/wallpapers/star-wars-broken-ship.jpg";
-      fade-in = 0.1;
+      fade-in = 1;
       clock = true;
       font = config.fontProfiles.regular.family;
-      font-size = 15;
+      font-size = 28;
       line-uses-inside = true;
       disable-caps-lock-text = true;
+
       indicator = true;
       indicator-caps-lock = true;
-      indicator-radius = 40;
+      indicator-radius = 100;
       indicator-idle-visible = true;
-      indicator-y-position = 1000;
+      indicator-thickness = 10;
+
+      color = "303446";
+      bs-hl-color = "f2d5cf";
+      caps-lock-bs-hl-color = "f2d5cf";
+      caps-lock-key-hl-color = "a6d189";
+      inside-color = "00000000";
+      inside-clear-color = "00000000";
+      inside-caps-lock-color = "00000000";
+      inside-ver-color = "00000000";
+      inside-wrong-color = "00000000";
+      key-hl-color = "a6d189";
+      layout-bg-color = "00000000";
+      layout-border-color = "00000000";
+      layout-text-color = "c6d0f5";
+      line-color = "00000000";
+      line-clear-color = "00000000";
+      line-caps-lock-color = "00000000";
+      line-ver-color = "00000000";
+      line-wrong-color = "00000000";
+      ring-color = "d3d3d3";
+      ring-clear-color = "f2d5cf";
+      ring-caps-lock-color = "ef9f76";
+      ring-ver-color = "8caaee";
+      ring-wrong-color = "ea999c";
+      separator-color = "00000000";
+      text-color = "c6d0f5";
+      text-clear-color = "f2d5cf";
+      text-caps-lock-color = "ef9f76";
+      text-ver-color = "8caaee";
+      text-wrong-color = "ea999c";
     };
   };
 }
