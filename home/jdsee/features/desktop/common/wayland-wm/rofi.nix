c4a384c4ef24453bfcaf9460bcb76491b9345472
jdsee <jdsee@protonmail.com>
2024-03-02 23:37:04 +0100

    Add some rofi themes

--- home/jdsee/features/desktop/common/wayland-wm/rofi.nix
+++ home/jdsee/features/desktop/common/wayland-wm/rofi.nix
@@ -4,9 +4,14 @@
   programs.rofi = {
     enable = true;
     package = pkgs.rofi-wayland;
+    plugins = with pkgs; [
+      rofi-rbw
+      rofi-power-menu
+    ];
+    theme = "${config.xdg.configHome}/rofi/rofi-themes-collection/themes/squared-everforest.rasi";
+    font = "${config.fontProfiles.regular.family} 12";
+    terminal = "${pkgs.foot}";
     cycle = true;
-    plugins = with pkgs; [ ];
-    theme = "android_notification";
   };
 
   home.packages = with pkgs; [
@@ -15,4 +20,16 @@
     rofi-power-menu
     bitwarden-menu
   ];
+
+  xdg.configFile = {
+    "rofi/rofi-themes-collection" = {
+      source = pkgs.fetchFromGitHub {
+        owner = "newmanls";
+        repo = "rofi-themes-collection";
+        rev = "5bc150394bf785b2751711e3050ca425c662938e";
+        hash = "sha256-k737CFrtbGfpqBxBOhrAD/QRC0aDryFs0jB4fycUooI=";
+      };
+      recursive = true;
+    };
+  };
 }
