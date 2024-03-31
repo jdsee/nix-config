e21a3751810f4419a89df6be8cfd0d09ef5f19f3
jdsee <jdsee@protonmail.com>
2024-03-31 20:56:42 +0200

    Fix nvim-substitute configuration

--- home/jdsee/features/nvim/config/nvim/lua/jdsee/plugins/substitue.lua
+++ home/jdsee/features/nvim/config/nvim/lua/jdsee/plugins/substitue.lua
@@ -1,20 +1,25 @@
 return {
   'gbprod/substitute.nvim',
-  dependencies = { 'gbprod/yanky.nvim' },
-  opts = {
-    -- on_substitute = require("yanky.integration").substitute(),
-    range = {
-      prefix = "gs",
-    },
-    highlight_substituted_text = {
-      timer = 200,
-    },
+  dependencies = {
+    'gbprod/yanky.nvim',
   },
   config = function()
     local substitute = require('substitute')
-    vim.keymap.set("n", "gs", substitute.operator, { noremap = true })
-    vim.keymap.set("n", "gss", substitute.line, { noremap = true })
-    vim.keymap.set("x", "gs", substitute.visual, { noremap = true })
-    vim.keymap.set("n", "S", substitute.eol, { noremap = true })
+
+    substitute.setup {
+      on_substitute = require('yanky.integration').substitute(),
+      range = {
+        prefix = 'gs',
+      },
+      highlight_substituted_text = {
+        enabled = true,
+        timer = 500,
+      },
+    }
+
+    vim.keymap.set('n', 'gs', substitute.operator, { noremap = true })
+    vim.keymap.set('n', 'gss', substitute.line, { noremap = true })
+    vim.keymap.set('x', 'gs', substitute.visual, { noremap = true })
+    vim.keymap.set('n', 'S', substitute.eol, { noremap = true })
   end
 }
